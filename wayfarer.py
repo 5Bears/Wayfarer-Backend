d9c3eeb5a7afd4f2030c4ce31de41314042633b8
asetoodehnia <asetoodehnia@gmail.com>
2016-11-13 06:57:24 -0800

    Flask

--- wayfarer.py
+++ wayfarer.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, abort, flash, jsonify
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
+
+
+
