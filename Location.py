a642a58ac684b54ee9d2f9e46c84ea7e5c063546
Adel Setoodehnia <Adel@airbears2-10-142-102-205.airbears2.1918.berkeley.edu>
2016-11-12 19:02:32 -0800

    implemented latitude and longitude

--- Location.py
+++ Location.py
@@ -1,18 +1,15 @@
-import json
-import urllib.parse
-import urllib.request
 from APIFunctions import build_url, get_result
-from uuid import getnode as get_mac
 
-mac = hex(get_mac())
-base_url = 'https://www.googleapis.com/geolocation/v1/geolocate?'
+base_url = 'https://maps.googleapis.com/maps/api/geocode/json?'
+address = '355 Roseland Place Union NJ'
 
-def format_mac(mac):
-	return mac[2] + mac[3] + ':' + mac[4] + mac[5] + ':' + mac[6] + mac[7] + ':' + mac[8] + mac[9] + ':' + mac[10] + mac[11] + ':' + mac[12] + mac[13]
+query_parameters = [('address', address),
+					('key', 'AIzaSyBlfi7_RqUEGq7yO39DvAbu2TYy4PEnszo')]
 
-query_parameters = [('key', 'AIzaSyCEFLuWX5CR7WXS5ELgT5dq3JwMDKL1pbc'),
-					('wifiAccessPoints', [('macAddress)', format_mac(mac))])]
+location = get_result(build_url(base_url, query_parameters))
 
-print(format_mac(mac))
-print(build_url(query_parameters, base_url))
-print(get_result(build_url(query_parameters, base_url)))
+def get_latitude(location):
+	return location["results"][0]["geometry"]["location"]["lat"]
+
+def get_longitude(location):
+	return location["results"][0]["geometry"]["location"]["lng"]
